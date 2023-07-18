7f01d0b333a79da1da9e0c8cfa8ec00dedf32fff
tubagus rivan <tubagus.rivan@inovasisolusi.com>
2023-07-18 15:23:13 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ app.use(express.json());
 routes(app);
 
 app.listen(3000, () =>
-  console.log("REST API server ready at: http://localhost:3000")
+  console.log("🚀 REST API server ready at: http://localhost:3000")
 );
